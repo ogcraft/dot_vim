ef1f571a4ef4ea654992ba995c525b9eda5fe9b5
Oleg Galbert <ogcraft@gmail.com>
2015-02-26 13:37:08 +0200

    _vimrc added

---
+++ bundle/get_bundles.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/scrooloose/syntastic.git
+git clone https://github.com/bling/vim-airline.git
+git clone https://github.com/venantius/vim-cljfmt.git
+git clone git://github.com/altercation/vim-colors-solarized.git
+git clone git://github.com/tpope/vim-fireplace.git
+git clone git://github.com/mkitt/tabline.vim.git
